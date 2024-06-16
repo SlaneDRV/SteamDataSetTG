b2e488c0020b1b952f72cc820eae867c2c466e73
SlaneDRV <125742851+SlaneDRV@users.noreply.github.com>
2024-06-16 20:37:32 +0200

    Update config.py

--- Project/TelegramBot/config.py
+++ Project/TelegramBot/config.py
@@ -1,10 +1,10 @@
 # Configuration file containing essential API keys and IDs
 
 # Telegram bot token used for authenticating the bot with Telegram API
-TOKEN = '7169861969:AAG4g7Wba_pl-zlksDs_14N3me3XWk6LoCE'
+TOKEN = ''
 
 # Steam API key used for accessing Steam's web API
-SteamKey = 'D350BB8AC6A45C05FA8B4EF538CEAE64'
+SteamKey = ''
 
 # Telegram user ID for identifying the authorized user
-TgID = 710194927
+TgID = 
